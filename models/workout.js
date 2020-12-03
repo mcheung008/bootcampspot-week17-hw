7eaa4dea1283117ea2038b8f50243a7f5b5963d6
Michael Cheung <mcheung008@gmail.com>
2020-12-02 20:32:18 -0800

    updated api routes and schema

--- models/workout.js
+++ models/workout.js
@@ -1,8 +1,62 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// define workout schema
 const workoutSchema = new Schema(
   {
-    // define workout schema
- }
+    day: {
+      type: Date,
+      default: Date.now
+    },
+
+    exercises: [
+      {
+        name: {
+          type: String,
+          trim: true,
+          required: 'Enter name of excercise!'
+        },
+
+        weight: {
+          type: Number,
+          required: 'Enter weight!'
+        },
+
+        sets: {
+          type: Number,
+          required: 'Enter number of sets!'
+        },
+
+        reps: {
+          type: Number,
+          required: 'Enter number of reps!'
+        },
+
+        duration: {
+          type: Number,
+          required: 'Enter duration!'
+        },
+
+        distance: {
+          type: Number,
+          required: 'Enter distance!'
+        }
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
 );
+
+workoutSchema.virtual('totalDuration').get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
+const Workout = mongoose.model('Workout', workoutSchema);
+
+module.exports = Workout;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const path = require('path');
+const Workout = require('./models/Workout');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -14,12 +15,17 @@ app.use(express.static('public'));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useFindAndModify: false,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
 }).then(() => console.log('mongodb connected'));
 
 // HTML routes, more than one
 app.get('/exercise', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/exercise.html'))
+});
+
+app.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/stats.html'))
 });
 
 // API routes
@@ -31,6 +37,40 @@ app.get('/api/workouts', (req, res) => {
         .catch(err => {
             res.json(err)
         })
+});
+
+app.get('/api/workouts/range', (req, res) => {
+    Workout.find()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+});
+
+app.post('/api/workouts', (req, res) => {
+    Workout.create({})
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+});
+
+app.put('/api/workouts/:id', ({body, params}, res) => {
+    Workout.findByIdAndUpdate(
+        params.id,
+        {$push:{exercises:body}},
+        {new: true, runValidators: true}
+    )
+    .then(data => {
+        res.json(data)
+    })
+    .catch(err => {
+        res.json(err)
+    })
 });
 
 
